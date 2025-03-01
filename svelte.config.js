188875eef9e99dbd3003ceb1b1da9d527badf2c5
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-01 15:44:21 -0600

    installed rehype and made img.svelte

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,9 @@ import { codeToHast, createHighlighter } from 'shiki';
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
 	highlight: {
 		highlighter: async (code, lang = 'text') => {
 			const highlighter = await createHighlighter({
