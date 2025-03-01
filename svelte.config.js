8cf84a16d59228973836f1ce4592f9a6207c4cad
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-01 15:22:27 -0600

    configured syntax highlighting with shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import { codeToHast, createHighlighter } from 'shiki';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['monokai'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'monokai' }))
+			return `{@html \`${html}\`}`
+		}
+	}
 }
 
 /** @type {import('@sveltejs/kit').Config} */
