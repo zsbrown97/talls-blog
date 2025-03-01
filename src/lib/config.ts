cf3162bba30586b0e5a6d95240386547a5990bd5
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-01 11:25:14 -0600

    followed joyofcode guide up to header and footer and css

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Shakespeare';
+export const description = 'Sveltekit blog for poets';
+export const url = 'http://localhost:5173';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,40 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+	import Footer from './footer.svelte'
+	import Header from './header.svelte'
+
+	import 'open-props/style'
+	import 'open-props/normalize'
+	import 'open-props/buttons'
+	import '../app.css'
+
+	let { children, data } = $props()
 </script>
 
-{@render children()}
+<div class="layout">
+	<Header />
+
+	<main>
+		{@render children?.()}
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+	.layout {
+		height: 100%;
+		max-inline-size: 1440px;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		margin-inline: auto;
+		padding-inline: var(--size-7);
+
+		@media (min-width: 1440px) {
+			padding-inline: 0;
+		}
+
+		main {
+			padding-block: var(--size-9);
+		}
+	}
+</style>
